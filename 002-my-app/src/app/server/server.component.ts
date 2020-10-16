6246d954d4f586265a839c3c581fe66c84c3fdf0
Stud. Daniel Justin Navone Verano <d.navoneverano.0808@vallauri.net>
2020-10-16 13:08:59 +0200

    ok

--- 002-my-app/src/app/server/server.component.ts
+++ 002-my-app/src/app/server/server.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { mintcream } from 'color-name';
 
 @Component({
     selector:'app-server',
     templateUrl: './server.component.html'
 })
 export class ServerComponent{
-    
+    serverId:number;
+    serverStatus:string;
+    constructor(){
+        this.serverId=this.getRandomInt(1,100)
+    }
+
+    getServerStatus(){
+        this.serverStatus = this.getRandomInt(0, 1) == 0 ? 'offline' : 'online';
+        return this.serverStatus;
+    }
+    getRandomInt(min, max){
+        return Math.floor(Math.random()*(max-min+1)) + min;
+    }
 }
